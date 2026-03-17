eaa1776bdf01ca0d405f96ed38c6107a256eb5c3
not rkorom <121223458+H4RmYxDD@users.noreply.github.com>
2026-03-17 10:40:47 +0100

    fos az egesz

---
+++ 260317/basic/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // runApp( MaterialApp(
+  //   title: "alap widgetek",
+  //   home: Scaffold(
+  //     body: Container( 
+  //         decoration:
+  //          BoxDecoration(
+  //           gradient: LinearGradient(colors: [const Color.fromARGB(255, 33, 112, 239),
+  //           // const Color.fromARGB(255, 6, 26, 59)),],),
+  //           // child: Text("heloo all")))),
+  //   ),
+  // );
+}
