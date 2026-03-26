6cae5f364c1126053da7c0f00109f9b9408f020a
not rkorom <121223458+H4RmYxDD@users.noreply.github.com>
2026-03-26 10:27:54 +0100

    wow ai

---
+++ 260326/example/lib/main.dart
@@ -0,0 +1,15 @@
+// import 'package:example/dropdown.dart';
+// import 'package:example/popup.dart';
+// import 'package:example/text_field.dart';
+// import 'package:example/checkbox.dart';
+// import 'package:example/checkbox_options.dart';
+// import 'package:example/text_form_field.dart';
+// import 'package:example/radio_button.dart';
+// import 'package:example/datetime_picker.dart';
+// import 'package:example/switch.dart';
+import 'package:example/table.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: TableExample()));
+}
